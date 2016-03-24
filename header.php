afd88603a635d415ee961bc6ea9f9e1cdcf4e2bd
mrdunster <mrdunster@wpi.edu>
2016-03-24 16:57:36 -0400

    Initial Upload

---
+++ header.php
@@ -0,0 +1,35 @@
+</head>
+<body>
+    
+    <div id="header">
+        
+        <div class="container">
+        
+        <h1><a href="index.html">Special Technical Services</a></h1>
+        
+        <div id="main_menu">
+        
+            <ul>
+                <li class="first_list"><a href="index.html" class="main_menu_first main_current">home</a></li>
+                <li class="first_list with_dropdown">
+                    <a href="products.html"  class="main_menu_first">products</a>
+                    <ul>
+                        <li class="second_list second_list_border"><a href="#" class="main_menu_second">ground monitoring system</a></li>
+                        <li class="second_list second_list_border"><a href="#" class="main_menu_second">grounding cables</a></li>
+                        <li class="second_list second_list_border"><a href="#" class="main_menu_second">grounding clamps</a></li>
+                        <li class="second_list second_list_border"><a href="#" class="main_menu_second">options</a></li>
+                        </ul>
+                </li>
+                <li class="first_list"><a href="#" class="main_menu_first">replacement parts</a></li>
+                </li>
+                <li class="first_list"><a href="#" class="main_menu_first">ordering guide</a></li>
+                <li class="first_list"><a href="#" class="main_menu_first">applications</a></li>
+                <li class="first_list"><a href="#" class="main_menu_first">technical info</a></li>
+                <!--li class="first_list"><a href="contact.html" class="main_menu_first">contact us</a></li-->
+                </ul>
+        
+        </div> <!-- END #main_menu -->
+    
+        </div> <!-- END .container -->
+    
+    </div> <!-- END #header -->
